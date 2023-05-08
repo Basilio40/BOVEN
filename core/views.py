c0be136d9297f552decafd6716c44f0b7c6cdaf9
sandrobispo <sbispo@otimaeg.com.br>
2023-05-08 10:33:27 -0300

    ajuste no csrf token

--- core/views.py
+++ core/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 
-# @login_required
+@login_required
 def index(request):
     return render(request, 'core/index.html')
 
