8e547eefca37559297b112cf57ada0bb600d6c62
sandrobispo <sbispo@otimaeg.com.br>
2023-05-08 10:45:16 -0300

    decorator csrf token

--- core/views.py
+++ core/views.py
@@ -6,8 +6,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.views.decorators.csrf import csrf_protect
 
 @login_required
+@csrf_protect
 def index(request):
     return render(request, 'core/index.html')
 
