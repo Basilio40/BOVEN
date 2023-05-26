86413bf364cafd03fbe1291dad319fc0f944bff6
sandrobispo <sbispo@otimaeg.com.br>
2023-05-26 16:06:55 -0300

    ajuste no csrf_token

--- core/views.py
+++ core/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
-from django.views.decorators.csrf import csrf_exempt
+
 
 # @login_required
 def index(request):
