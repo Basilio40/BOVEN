740ddbdbbb6e7d1a10c8d8b232c908b7713ec41b
sandrobispo <sbispo@otimaeg.com.br>
2023-05-26 15:40:24 -0300

    mudanção do securit SSL

--- calc_energia/settings.py
+++ calc_energia/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 ROOT_URLCONF = 'calc_energia.urls'
 
